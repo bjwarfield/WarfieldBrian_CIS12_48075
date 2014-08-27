8e84b582c497c879f537f83ed49000eef33c62a6
bwarfield <itsabeejay@yahoo.com>
2014-08-27 09:22:37 -0700

    Signed-off-by: bwarfield <itsabeejay@yahoo.com>

---
+++ Class/strings_and_variables/strigs_and_variables.php
@@ -0,0 +1,28 @@
+<?php
+	/*
+	Brian Warfield
+	27 August 2014
+	Purpose: Comments/Variables/Strings
+	*/
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Strings and Variables</title>
+</head>
+<body>
+	<?php
+		//Declare Variables
+		$hours=40;//Worked 40 hours
+		$payRate=9;//$'s/hour
+		
+		//Calculate Paycheck
+		$grossPay=$hours*$payRate;
+		//Output the result
+		echo "<p>Hours worked = $hours (hrs)</p>";
+		echo '<p>Pay Rate = $$payRate per hour</p>';
+		echo "<p>Pay Check = $".$grossPay.'</p>';
+	?>
+</body>
+</html>
