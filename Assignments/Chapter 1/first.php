b9ec48caf1b073e14625f5fb2af39c4c0a20028e
bwarfield <itsabeejay@yahoo.com>
2014-08-27 16:24:38 -0700

    Signed-off-by: bwarfield <itsabeejay@yahoo.com>

--- Assignments/Chapter 1/first.php
+++ Assignments/Chapter 1/first.php
@@ -5,6 +5,7 @@
 	27 August 2014
 	Purpose: Script 1.2
 	*/
+	//check
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
