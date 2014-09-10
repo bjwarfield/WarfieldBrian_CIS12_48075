007cc8ee849e24a27fc37d755e38b3178d18b6cb
bwarfield <itsabeejay@yahoo.com>
2014-09-10 08:13:37 -0700

    Signed-off-by: bwarfield <itsabeejay@yahoo.com>

---
+++ Lab/Lab_1/Lab_1.1.php
@@ -0,0 +1,22 @@
+<?php
+	/*
+	Brian Warfield
+	CIS 12 PHP
+	10 September 2014
+	Purpose: Lab Assignment 1.1 - Codecademy lesson 1-13
+	*/
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link type='text/css' rel='stylesheet' href='style.css'/>
+		<title>PHP FTW!</title>
+	</head>
+	<body>
+        <!-- Write your PHP code below!-->
+        <p>
+            <?php $myName = "BeeJay"; $myAge = 30;
+            echo $myName; echo $myAge; ?>
+        </p>   
+	</body>
+</html>
---
+++ Lab/Lab_1/Lab_1.2.php
@@ -0,0 +1,26 @@
+<?php
+	/*
+	Brian Warfield
+	CIS 12 PHP
+	10 September 2014
+	Purpose: Lab Assignment 1.1 - Codecademy lesson 2-4
+	*/
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Control Flow</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        $num = 6   ;
+        if ($num < 5) {
+          echo "The condition is true";
+        }else{
+            echo "The condition is false";
+        }
+      ?>
+    </p>
+  </body>
+</html>
