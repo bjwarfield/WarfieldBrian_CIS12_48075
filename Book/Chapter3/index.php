95373363c5d05075d8e005e55904cda635ea70c6
bwarfield <itsabeejay@yahoo.com>
2014-10-07 14:14:42 -0700

    Signed-off-by: bwarfield <itsabeejay@yahoo.com>

---
+++ Book/Chapter3/index.php
@@ -0,0 +1,10 @@
+<?php //script 3.4 index.php
+	$page_title = "This page will be full of meat";
+	include ("includes/header.html");
+?>
+	<h1>Some stuff</h1>			
+	<p>This is where the page-specific content goes. This section, and the corresponding header, will change from one page to the next.</p>
+	<p>Bacon ipsum dolor sit amet proident ham hock prosciutto irure qui t-bone kielbasa tri-tip nulla turducken. Dolore ullamco meatball shankle veniam et. Ex kielbasa ham turducken mollit. Dolore labore bacon pork loin, nisi frankfurter ut commodo prosciutto sed. Duis et meatball reprehenderit. Biltong boudin tempor in.</p>
+<?php 
+	include("includes/footer.html");
+?>
